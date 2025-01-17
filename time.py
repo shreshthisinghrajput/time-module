b0a3089c8fd91aaafc22ef4ea8a4390575fa627f
maggieshaggy <156869895+shreshthisinghrajput@users.noreply.github.com>
2025-01-17 08:28:25 +0530

    Add files via upload

--- time.py
+++ time.py
@@ -1,34 +1,31 @@
 #differnce between for and while loop
 import time
-# def using_for():
-#     for i in range(5):
-#         print(i)
-#         i=i+1
-# def using_while():
-#     i=0
-#     while i<5:
-#         print(i)
-#         i=i+1
-# t1=time.time()
-# using_for()
-# t2=time.time()
-# using_while()
-# print(time.time()-t1)
-# print(time.time()-t2)
+def using_for():
+    for i in range(5):
+        print(i)
+        i=i+1
+def using_while():
+    i=0
+    while i<5:
+        print(i)
+        i=i+1
+t1=time.time()
+using_for()
+t2=time.time()
+using_while()
+print(time.time()-t1)
+print(time.time()-t2)
 
 # simple alarm clock
 
-
-# i=int(input("set the time in seconds"))
-# time.sleep(i)
-# print("time is up")
+i=int(input("set the time in seconds"))
+time.sleep(i)
+print("time is up")
 
 #countdowntimer
 
 facemask=print("facemask") 
-
 boil_egg=print("boil_egg")
-
 rice=print("rice")
 i=input("enter the stuff you wanna do ")
 if i=="facemask":
